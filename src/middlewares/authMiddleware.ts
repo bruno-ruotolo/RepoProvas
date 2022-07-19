b8ccc2a807601148470838806c71d4043ecfafed
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-07-19 09:36:08 -0300

    fix: envExamples and authService

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
+import authService from "../repositories/authService.js";
 import { unauthorizedError } from "../utils/errorUtils.js";
 
 export default async function authMiddleware(req: Request, res: Response, next: NextFunction) {
---
+++ src/repositories/authService.ts
@@ -0,0 +1,9 @@
+export async function findUserById(id: number) {
+  return "Here Will Return the User"
+};
+
+const authService = {
+  findUserById
+};
+
+export default authService;
